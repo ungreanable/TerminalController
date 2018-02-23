725955a1f50158f0f82b26fe44ff1479fad2a66a
ungreanable <boosadz@gmail.com>
2018-02-23 08:50:43 +0700

    Add bin folder (Start Terminal, Kill All Terminal, Toggle EA), edit README.md

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ForexController
+Controller your Terminal by ForexController
+- Start All Terminal
+- Kill All Terminal
+- Toggle Expert Advisors (EA)
+- Toggle FreezeAfterTP (work with EA #1)
+
+You can download EA at #1
+
+#1 http://forextraderportal.com/forex-grid-trader-ea-making-money-from-your-bed/
---
+++ bin/start terminal.bat
@@ -0,0 +1,8 @@
+start /MAX /D "C:\Program Files (x86)\XM Global MT4\" terminal.exe
+start /MAX /D "C:\Program Files (x86)\XM Global MT4 - 2\" terminal.exe
+start /MAX /D "C:\Program Files (x86)\XM Global MT4 - 3\" terminal.exe
+start /MAX /D "C:\Program Files (x86)\XM Global MT4 - 4\" terminal.exe
+start /MAX /D "C:\Program Files (x86)\XM Global MT4 - 5\" terminal.exe
+start /MAX /D "C:\Program Files (x86)\XM Global MT4 - 6\" terminal.exe
+start /MAX /D "C:\Program Files (x86)\XM Global MT4 - 7\" terminal.exe
+start /MAX /D "C:\Program Files (x86)\XM Global MT4 - 8\" terminal.exe
