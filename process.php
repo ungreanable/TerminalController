20311a7045649a2bd9043797cb7d9380f1c9d8ac
ungreanable <boosadz@gmail.com>
2018-02-28 18:37:31 +0700

    Add Basic Authentication System

--- process.php
+++ process.php
@@ -1,24 +1,29 @@
 <?php
 $command = $_GET['command'];
-if($command == "startTerminal") {
-	$result['output'] = shell_exec("bin\start terminal.bat");
-} else if($command == "killTerminal") {
-    $result['output'] = shell_exec("bin\kill terminal.bat");
-} else if($command == "toggleEA") {
-    $result['output'] = shell_exec("bin\Terminal Controller.exe");
-} else if($command == "freezeAfterTP") {
-		$my_file = 'settings/settings.txt';
-	if(isset($_GET['setValue'])) {
-		$handle = fopen($my_file, 'w');
-		fwrite($handle, $_GET['setValue']);
-		$result['output'] = $_GET['setValue'];
-	} else {
-		$handle = fopen($my_file, 'r');
-		$data = fread($handle,filesize($my_file));
-		echo $data;
-	}
+$my_file = 'settings/settings.txt';
+if(isset($_GET['key'])) {
+	$key = $_GET['key'];
+	if($key == "") {
+		if($command == "startTerminal") {
+			$result['output'] = shell_exec("bin\start terminal.bat");
+		} else if($command == "killTerminal") {
+			$result['output'] = shell_exec("bin\kill terminal.bat");
+		} else if($command == "toggleEA") {
+			$result['output'] = shell_exec("bin\Terminal Controller.exe");
+		} else if($command == "freezeAfterTP") {
+			if(isset($_GET['setValue'])) {
+				$handle = fopen($my_file, 'w');
+				fwrite($handle, $_GET['setValue']);
+				$result['output'] = $_GET['setValue'];
+				fclose($handle);
+			}
+		}
+	} else $result['output'] = "R U OK ?";
+} else if($command == "freezeAfterTP" && !isset($_GET['setValue'])) {
+	$handle = fopen($my_file, 'r');
+	$data = fread($handle,filesize($my_file));
+	echo $data;
 	fclose($handle);
 }
-
 if($command != "freezeAfterTP" || isset($_GET['setValue'])) echo json_encode($result);
 ?>
